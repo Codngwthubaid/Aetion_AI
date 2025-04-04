35ea9bcd22a9befed55ff621ce702d00781d21c9
Codngwthubaid <codngwthubaid@gmail.com>
2025-04-04 11:52:20 +0530

    working on UI - dashboard

---
+++ by-AmazonPolly/app/(main)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function  Dashboard(){
+    return(
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit repellendus quia dolores voluptatem amet tempora eaque! Ab numquam a magni illum nemo facilis, veniam, perferendis est id quia temporibus nesciunt.</div>
+    )
+}
