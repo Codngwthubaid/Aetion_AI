f9c5e2212ef0d62e4424f6aa646543df6937161d
Codngwthubaid <codngwthubaid@gmail.com>
2025-05-01 00:06:43 +0530

    init nextjs Application and authentication and database attached successfuly

---
+++ newer/app/provider.tsx
@@ -0,0 +1,45 @@
+"use client"
+import { UserDetailsContext } from "@/context/UserDetailsContext";
+import { supabase } from "@/services/supabaseClient";
+import { useContext, useEffect, useState } from "react";
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+
+    const [user, setUser] = useState<string | null>()
+    useEffect(() => { createNewUser() }, [])
+
+    const createNewUser = () => {
+        supabase.auth.getUser().then(async ({ data: { user } }) => {
+            let { data: Users, error } = await supabase
+                .from('Users')
+                .select("*")
+                .eq('email', user?.email)
+
+            console.log(Users);
+
+            if (Users?.length == 0) {
+                const { data, error } = await supabase.from('Users').insert(
+                    [
+                        {
+                            name: user?.user_metadata?.name,
+                            email: user?.email,
+                            picture: user?.user_metadata?.picture
+                        }
+                    ]
+                )
+                console.log(data);
+                setUser(data)
+                return;
+            }
+            if (Users) { setUser(Users[0]) }
+        })
+    }
+
+    return (
+        <UserDetailsContext.Provider value={{ user, setUser }}>
+            <div>{children}</div>
+        </UserDetailsContext.Provider>
+    );
+}
+
+export const useUser = () => { return useContext(UserDetailsContext) }
